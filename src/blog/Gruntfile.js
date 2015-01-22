8de027ea2691c82b2a06eb3b2d5b59593e7bbae4
Kendall Clark <kendall@clarkparsia.com>
2015-01-22 12:20:04 -0500

    adding blog impl

--- src/blog/Gruntfile.js
+++ src/blog/Gruntfile.js
@@ -7,132 +7,93 @@ module.exports = function(grunt) {
     // Time how long tasks take. Can help when optimizing build times
     require('time-grunt')(grunt);
 
-  // Project configuration.
-  grunt.initConfig({
-      stylus: {
+    // Project configuration.
+    grunt.initConfig({
+        stylus: {
           compile: {
               options: {
-                  paths: ["node_modules/axis/axis/"  
+                  compress: true,
+                  paths: ["node_modules/axis/",
+                          //"node_modules/jeet/stylus/jeet",
+                          "node_modules/jeet/stylus/",
+                          "node_modules/rupture/",
                   ],
                   urlfunc: "embedurl",
               },
               files: {
-                  "static/s.css": "styl/s.styl"
+                  "static/css/s.css": "styl/s.styl"
               }
           }
-      },
-      uncss: {
-          dist: {
-              files: {
-                  'website/stardog.css': ['website/index.html']
+        },
+         shell: { //me with ascidoctor.js
+          build: {
+              command: function () {
+                  comm = "hugo"
+                  return comm
               }
           }
-      },
+        },
+        //WARNING: never put this in a git repo dir... put it outside the repo!
+        aws: grunt.file.readJSON("../../../grunt-aws-SECRET.json"),
+        //tell grove we want this bucket to be in > 1 AZ...yes?
+        //need another bucket for staging...doesn't need DNS (or ?)...it can be in 1 AZ
+        //fork hugo to build asciidoctor and call it...Boris!
+        aws_s3: {
+            dev: {
+                options: {
+                    accessKeyId: "<%= aws.secret %>", 
+                    secretAccessKey: '<%= aws.key %>',
+                    bucket: "<%= aws.bucket %>",
+                    region: "us-east-1",
+                    access: "public-read",
+                    progress: "progressBar",
+                    streams: true,
+                    debug: false,
+                    uploadConcurrency: 30,
+                    params: {
+                        "CacheControl": "max-age=63072000, public",
+                        "Expires": new Date(Date.now() + 6.31139e10),//.toUTCString(),
+                        //"ContentEncoding": "gzip"
+                        
+                    }
+                },
+                files: [
+                    { expand: true, dest: '.', cwd: 'public/', src: ['**'], action: 'upload', differential: true },
+                    { dest: '/', cwd: 'public/', action: 'delete', differential: true }
+                ]
+            }
+        },
+        compress: {
+            main: {
+                options: { mode: 'gzip'},
+                expand: true,
+                cwd: "public/",
+                src: ["**/*"],
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                expand: true,
+                cwd: 'public',
+                src: ['**/*.html'],
+                dest: 'public/'
+            }
+        },
       availabletasks: {     
           tasks: {}
       },
-      copy: {
-          main: {
-              nonull: true,
-              cwd: 'doc/',
-              src: 'icv/*',
-              dest: 'website/',
-              expand: true,
-          },
-          icv_img: {
-              nonull: true,
-              cwd: 'doc/optimized-img',
-              src: 'ClassDiagram.png',
-              dest: 'website/icv/',
-              expand: true
-          },
-          css: {
-              nonull:true,
-              src: 'style/stylesheets/stardog.css',
-              dest: 'website/stardog.css',
-          },
-          pub: {
-              nonull:true,
-              src:'**',
-              dest:'../published',
-              expand: true,
-              cwd: 'website/'
-          }
-      },
-      htmlmin: {                                 
-          dist: {                                     
-              options: {                               
-                  removeComments: true,
-                  collapseWhitespace: true
-              },
-              files: {
-                  'website/index.html': 'website/index.html',    
-              }
-          },
-      },
-       open: {
-           dev: {
-               path: 'http://127.0.0.1:8001',
-               app: 'Safari',
-           },
-           prod: {
-               path : 'http://docs.stardog.com/',
-               app: 'Google Chrome'
-           },
-      },
-      embed: {
-          dist: {
-              options: {
-                  stylesheets: true,
-              },
-              files: {
-                  "website/index.html": "website/index.html"
-              }
-          }
-      },
-      cssmin: {
-          dist: {
-              files: {'website/stardog.css': ['website/stardog.css']}
-          }
-      },
       clean: {
-          css: ["website/stardog.css"],
-          css2: ["style/stylesheets/stardog.css"],
-          build: ["website"],
-          release: ["doc/optimized-img"]
-      },
-      inline: {
-          dist: {
-              options:{
-                  tag: 'img'
-              },
-              src: 'website/index.html',
-              dest: 'website/index.html'
-          }
-      },
-      replace: {
-          main: {
-              src: ['website/index.html'],
-              overwrite: true,
-              replacements: [{
-                  from: "</title>",
-                  to: "</title><link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,400italic|Anonymous+Pro:400,400italic' rel='stylesheet' type='text/css'>"
-              },{
-                  from: '<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.4/highlight.min.js"></script>',
-                  to: ''
-              },{
-                  from: '<link rel="stylesheet" href="./stardog.css">',
-                  to: '<link data-embed rel="stylesheet" href="./stardog.css">'
-              },{
-                  from: '<script>hljs.initHighlightingOnLoad()</script>',
-                  to: ''
-              }, {
-                  from: '</body>',
-                  to: '<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.4/highlight.min.js"></script>\n<script>hljs.initHighlightingOnLoad()</script>\n</body>'
-              }]
-          }
+          build: ["public/*"],
       },
   });
 
     require('matchdep').filter('grunt-*').forEach(grunt.loadNpmTasks);
+    grunt.registerTask('cl', ['clean:build']);
+    grunt.registerTask("css", ["stylus"]);
+    grunt.registerTask('dev', ['clean:build', 'stylus', 'shell']);
+    grunt.registerTask("pub", ['clean:build', "stylus", "shell", "htmlmin", 'aws_s3']);
 };
