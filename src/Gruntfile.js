7e1f909a743ad250a30ae9dc7b7bf789782e18d7
Kendall Clark <kendall@clarkparsia.com>
2015-02-18 14:04:10 -0500

    pass release id and date to asciidoctor from CLI

--- src/Gruntfile.js
+++ src/Gruntfile.js
@@ -1,10 +1,10 @@
 //require('load-grunt-tasks')(grunt);
-var theId = "3.0";
-var theDate = '"28 February 2015"';
 
 module.exports = function(grunt) {
     // Load grunt tasks automatically
     require('load-grunt-tasks')(grunt);
+    var theId = grunt.option("id") || "3.0";
+    var theDate = grunt.option("date") || "'28 February 2015'";
 
     // Time how long tasks take. Can help when optimizing build times
     require('time-grunt')(grunt);
