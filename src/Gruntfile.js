49c1dd23926a2b21a124ff01ebef0769a0fdd06c
Kendall Clark <kendall@clarkparsia.com>
2015-05-05 09:56:20 -0400

    updates

--- src/Gruntfile.js
+++ src/Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
     // Load grunt tasks automatically
     require('load-grunt-tasks')(grunt);
     var theId = grunt.option("id") || "3.0.1";
-    var theDate = grunt.option("date") || "'13 April 2015'";
+    var theDate = grunt.option("date") || "'14 April 2015'";
 
     // Time how long tasks take. Can help when optimizing build times
     require('time-grunt')(grunt);
