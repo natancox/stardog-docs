ed761e142f1e92f58b85865d7b6fb3b527caf1fe
Kendall Clark <kendall@clarkparsia.com>
2015-10-28 10:02:29 -0400

    getting ready for rc3

--- src/Gruntfile.js
+++ src/Gruntfile.js
@@ -3,8 +3,8 @@
 module.exports = function(grunt) {
     // Load grunt tasks automatically
     require('load-grunt-tasks')(grunt);
-    var theId = grunt.option("id") || "4.0RC2";
-    var theDate = grunt.option("date") || "20 October 2015";
+    var theId = grunt.option("id") || "4.0RC3";
+    var theDate = grunt.option("date") || "29 October 2015";
 
     // Time how long tasks take. Can help when optimizing build times
     require('time-grunt')(grunt);
